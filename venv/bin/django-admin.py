316b3fdff20935a838db92c5db561b40ad97a4eb
bsmolley <bsmolley@ualberta.ca>
2016-03-03 10:44:49 -0700

    .gitignore added, removed venv

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/cshome/bsmolley/c404/Project/CMPUT404-project-socialdistribution/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
