eb1bd91cdff3627625478d39859602b0f2b0ea10
bsmolley <bsmolley@ualberta.ca>
2016-03-10 14:47:19 -0700

    Settings now save

--- Ditto/settings/views.py
+++ Ditto/settings/views.py
@@ -26,8 +26,15 @@ def settings(request):
 
 	# github flag
 	github_flag = author_object.github_flag
+	print github_flag
+
 	if github_flag:
 		github_flag = 'checked'
+		print github_flag
+	else:
+		github_flag = ''
+
+
 
 	context = {
 		'display_name': display_name,
@@ -36,3 +43,33 @@ def settings(request):
 	}
 
 	return render(request, 'settings.html', context)
+
+def save_settings(request):
+	user_object = User.objects.get(username=request.user.username)
+	author_object = Author.objects.get(email = user_object)
+	display_name = request.POST.get('display_name')
+	new_password = request.POST.get('new_password').strip()
+	retype = request.POST.get('retype').strip()
+	github_account = request.POST.get('github_name')
+	github_flag = request.POST.get('test6')
+
+	if github_flag == 'true':
+		github_flag = True
+	elif github_flag == 'false':
+		github_flag = False
+
+	if len(new_password) > 0 and len(retype > 0):
+		if new_password == retype:
+			password = new_password
+			print password
+			user_object.set_password(new_password)
+
+
+	author_object.display_name = display_name
+	author_object.github_account = github_account
+	author_object.github_flag = github_flag
+
+	user_object.save()
+	author_object.save()
+
+	return HttpResponse(request)
