c785f56846e8000b36671d5d4331a6c2087bd1e1
bsmolley <bsmolley@ualberta.ca>
2016-03-19 14:24:47 -0600

    Added all friends

--- friends/views.py
+++ friends/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from feed.models import Author
+
 
 @login_required
 # Create your views here.
 def friends(request):
-	return render(request, 'friends.html')
+	all_authors = Author.objects.all()
+	context = {
+		'authors': all_authors,
+	}
+	return render(request, 'friends.html', context)
