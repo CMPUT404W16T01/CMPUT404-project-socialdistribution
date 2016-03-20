a7b5ed0110d0328ffdb3a1c72c45d9fac72b1947
Michael Stensby <michael.stensby@gmail.com>
2016-03-19 22:42:00 -0600

    Trying to fix error

--- api/serializers.py
+++ api/serializers.py
@@ -2,20 +2,6 @@ from rest_framework import serializers
 from feed.models import Post, Comment, Author
 
 
-class PostSerializer(serializers.ModelSerializer):
-    author = AuthorSerializer()
-    comments = serializers.SerializerMethodField('get_comments')
-
-    def get_comments(self, obj):
-        return CommentSerializer(Comment.objects.filter(post=obj.id), many=True).data
-
-    class Meta:
-        model = Post
-        fields = (
-            'title', 'source', 'origin', 'description', 'contentType', 'content', 'author', 'comments' 'categories',
-            'published', 'id', 'visibility')
-
-
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
@@ -32,3 +18,17 @@ class CommentAuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = ('id', 'host', 'displayName', 'github', 'bio')
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer()
+    comments = serializers.SerializerMethodField('get_comments')
+
+    def get_comments(self, obj):
+        return CommentSerializer(Comment.objects.filter(post=obj.id), many=True).data
+
+    class Meta:
+        model = Post
+        fields = (
+            'title', 'source', 'origin', 'description', 'contentType', 'content', 'author', 'comments' 'categories',
+            'published', 'id', 'visibility')
