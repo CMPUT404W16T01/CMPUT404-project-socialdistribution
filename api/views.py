7e9d1d2609e99072a1cee226f60cdc4fd4d13156
bsmolley <bsmolley@ualberta.ca>
2016-03-20 17:28:43 -0600

    Friends requests now route properly

--- api/views.py
+++ api/views.py
@@ -184,15 +184,25 @@ class friend_request(APIView):
         friend_to_author = Friend.objects.filter(follower_id=friend["id"], followed_id=author["id"])
         author_to_friend = Friend.objects.filter(follower_id=author["id"], followed_id=friend["id"])
         # checks what kind of relationship the two have, intimate or otherwise
+
+        # are they following me?
+        print len(friend_to_author)
+
+        # am I followign them
+        print len(author_to_friend)
+
         if (len(friend_to_author) == 1) and (len(author_to_friend) == 1):
             print "you're an idiot you're already friends"
+
         elif (len(friend_to_author) == 1) and (len(author_to_friend) == 0):
-            new_friend_object = Friend(follower_id=friend["id"], followed_id=author["id"],
-                                       follower_host=friend_host, followed_host=author_host)
+            new_friend_object = Friend(follower_id=author["id"], followed_id=friend["id"],
+                                       follower_host=author_host, followed_host=friend_host)
             new_friend_object.save()
             # WE ARE NOW FRIENDS
+
         elif (len(friend_to_author) == 0) and (len(author_to_friend) == 1):
             pass
+
         elif (len(friend_to_author) == 0) and (len(author_to_friend) == 0):
             new_friend_object = Friend(follower_id=author["id"], followed_id=friend["id"], follower_host=author_host,
                                        followed_host=friend_host)
@@ -202,17 +212,19 @@ class friend_request(APIView):
         # CHECK THE USERS, IF ONE OF THEM IS OFF SERVER WE MUST POST TO THEIR SERVER
         print request.get_host()
 
-        try:
-            #url = 'http://' + 'localhost:8001' + '/api/friendrequest'
-            url = 'http://mighty-cliffs-82717.herokuapp.com/api/friendrequest'
-            print "beep"
-            packet = {"query":"friendrequest", "author":author, "friend":friend }
-            print url
-            print "boop"
-            r = requests.post(url, json=packet)
-            print "ahhh we sent something"
-        except:
-            pass
+        # this gets called even when we don't need to send them requests
+        if ("mighty-cliffs-82717" in author_host) or ("mighty-cliffs-82717" in friend_host):
+            try:
+                #url = 'http://' + 'localhost:8001' + '/api/friendrequest'
+                url = 'http://mighty-cliffs-82717.herokuapp.com/api/friendrequest'
+                print "beep"
+                packet = {"query":"friendrequest", "author":author, "friend":friend }
+                print url
+                print "boop"
+                r = requests.post(url, json=packet)
+                print "ahhh we sent something"
+            except:
+                pass
 
 
         return Response()
