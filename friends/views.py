5bd87f8450a95a21faeb879731b3ac6c7e7ecfb6
bsmolley <bsmolley@ualberta.ca>
2016-03-22 12:53:24 -0600

    test friend

--- friends/views.py
+++ friends/views.py
@@ -45,8 +45,10 @@ def friends(request):
 				req = urllib2.Request("http://mighty-cliffs-82717.herokuapp.com/api/author/%s" % str(i.follower_id))
 				print "attempting mighty request"
 				response = urllib2.urlopen(req).read()
+				print response
 				loaded = json.loads(response)
 				print loaded
+				friend = loaded
 
 			if friend.id != author_object.id and not loaded.get('friends'):
 				friend_requests.append(friend)
