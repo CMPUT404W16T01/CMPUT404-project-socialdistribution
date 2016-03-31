eb71d06a7bc9f451864cee3fe540cd60cb0c0dbb
Randy Wong <rtwong@ualberta.ca>
2016-03-30 23:25:46 -0600

    ahhh commit spams15

--- feed/views.py
+++ feed/views.py
@@ -233,7 +233,7 @@ def get_profile(request, pk):
 
 
     url = them_host + "api/author/" + them_id + "/posts?id=" + str(us_object.id)
-    url = "http://ditto-test.herokuapp.com/api/authors"
+    url = them_host + "api/author/" + them_id + "/posts"
     print url
     req = urllib2.Request(url)
 
