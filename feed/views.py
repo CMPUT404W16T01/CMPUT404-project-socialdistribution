9c62ec19b695de561b2e5f5e9fefcc18a4de54ff
bsmolley <bsmolley@ualberta.ca>
2016-04-02 15:16:37 -0600

    whack a mole2

--- feed/views.py
+++ feed/views.py
@@ -605,7 +605,7 @@ def create_comment(request):
 
     if 'ditto-test' in origin:
         url1 = origin + "/comments/"
-    elif "4" in origin:
+    elif 'mighty' not in origin:
         url1 = "http://project-c404.rhcloud.com/api/posts/" + parent_id + "/comments" 
     else:
         url1 = origin + "/api/posts/" + parent_id + "/comments"
