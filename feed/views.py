aa7405ea439fd6ea0b01d81a76035f5e26cd5fae
bsmolley <bsmolley@ualberta.ca>
2016-04-02 13:15:13 -0600

    this is fine

--- feed/views.py
+++ feed/views.py
@@ -53,7 +53,7 @@ def feed(request):
     their_post_list = []
     try:
         foreign_hosts = ForeignHost.objects.filter()
-        #their_post_list, public_post_list = getOurShit(author_object)
+        their_post_list, public_post_list = getOurShit(request, author_object)
         
         for i in foreign_hosts:
             # url = i.url + "api/author/posts?id=" + str(author_object.id) 
@@ -235,17 +235,125 @@ def feed(request):
 
     return render(request, 'feed.html', context)
 
-def getOurShit(author_object):
-    url = author_object.host + "api/author/posts?id=" + str(author_object.id)
-    req = urllib2.Request(url)
+def getOurShit(request, author_object):
+    
+    # url = author_object.host + "api/author/posts?id=" + str(author_object.id)
+    # req = urllib2.Request(url)
 
-    base64string = base64.encodestring('%s:%s' % ("admin", "pass")).replace('\n', '')
-    req.add_header("Authorization", "Basic %s" % base64string) 
+    # base64string = base64.encodestring('%s:%s' % ("admin", "pass")).replace('\n', '')
+    # req.add_header("Authorization", "Basic %s" % base64string) 
 
-    response = urllib2.urlopen(req).read()
-    loaded = json.loads(response)
+    # response = urllib2.urlopen(req).read()
 
-    our_posts = loaded.get('posts')
+
+
+
+    #---------------
+    asker_host = request.META.get("HTTP_HOST")
+
+    try:
+        asker_object = Author.objects.get(email=request.user)
+        asker_id = str(asker_object.id)
+    except:
+        asker_id = request.GET.get('id', default=None)
+        asker_id = asker_id.strip("/")
+        if asker_id == None:
+            return Response({"details": "give and ?id=xxxx"}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            asker_id = str(asker_id)
+
+
+
+    public_posts = Post.objects.filter(visibility="PUBLIC")
+    return_posts = public_posts
+
+
+    all_authors = Author.objects.filter()
+
+    for each in all_authors:
+        each_id = str(each.id)
+
+        # the asker is the user itself, add in what only they could see
+        if (each_id == asker_id):
+            private_posts = Post.objects.filter(author=asker_object, visibility="PRIVATE")
+            return_posts = return_posts | private_posts
+            continue
+      
+        # if the asker is a friend
+        friend_to_author = Friend.objects.filter(follower_id=each_id, followed_id=asker_id)
+        author_to_friend = Friend.objects.filter(follower_id=asker_id, followed_id=each_id)
+
+        if (len(friend_to_author) == 1) and (len(author_to_friend) == 1):
+            #then they are friends, because the relationship is mutual
+            friend_posts = Post.objects.filter(author=each, visibility="FRIENDS")
+            return_posts = return_posts | friend_posts
+
+        # if the asker is on our server, and a friend
+        if (len(Author.objects.filter(id=asker_id)) > 0) and (len(friend_to_author) == 1) and (len(author_to_friend) == 1):
+            server_friends_posts = Post.objects.filter(author=each, visibility="SERVERONLY")
+            return_posts = return_posts | server_friends_posts
+
+        # TODO: Look at FOAF stuff
+        # asker_id is person A
+        # as ditto, we need to ask person A's host who A is friends with
+
+        # fetch list of A's friends
+        url = "http://" + asker_host + "/api/friends/" + asker_id
+        req = urllib2.Request(url)
+
+
+        # assume we are sending to ourselves to begin with, if we are getting this from
+        # another host then we will update after
+        base64string = base64.encodestring('%s:%s' % ("admin", "pass")).replace('\n', '')
+        req.add_header("Authorization", "Basic %s" % base64string)
+
+
+        foreign_hosts = ForeignHost.objects.filter()
+        for host in foreign_hosts:
+            # if the sender host, which is a clipped version of the full host path, is part of it, then that host
+            # is the correct one we're looking for
+            if asker_host in host.url:
+                base64string = base64.encodestring('%s:%s' % (host.username, host.password)).replace('\n', '')
+                req.add_header("Authorization", "Basic %s" % base64string)
+
+
+        response = urllib2.urlopen(req).read()
+        loaded = json.loads(response)
+
+
+        # we now have a list of authors who are friends with the asker
+        # if we are friends with any of them then we can give them our FOAF marked posts
+        # or if we were friends to begin with they can also see FOAF marked posts
+
+        for author in loaded['authors']:
+            # if we are directly friends lets just give it to them
+            if ((len(friend_to_author) == 1) and (len(author_to_friend) == 1)) or (each_id == asker_id):
+                foaf_posts = Post.objects.filter(author=each, visibility="FOAF")
+                return_posts = return_posts | foaf_posts
+                break
+            else:
+                # we should check if we are friends of any of A's friends
+                #author is a string of a uuid
+                a_to_b = Friend.objects.filter(follower_id=asker_id, followed_id=author)
+                b_to_a = Friend.objects.filter(follower_id=author, followed_id=asker_id)
+                if (len(a_to_b) == 1) and (len(b_to_a) == 1):
+                    # we are friends with one of their friends
+                    foaf_posts = Post.objects.filter(author=each, visibility="FOAF")
+                    return_posts = return_posts | foaf_posts
+                    break
+
+
+
+    # we need to get all the posts 
+    #serializer = PostSerializer(return_posts, many=True)
+
+
+    #---------------
+    #loaded = json.loads(response)
+
+
+    our_posts = return_posts
+    #our_posts = loaded.get('posts')
 
     return_public_posts = []
     return_main_posts = []
